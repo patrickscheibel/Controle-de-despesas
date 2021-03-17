42a912003252677f8fe9a8014a190db6ca9dbd8d
patrick <patrick@email.com>
2021-03-17 02:21:37 -0300

    ajustes nas telas do CRUD de categoria

--- adicionar.php
+++ adicionar.php
@@ -4,11 +4,11 @@
     <head>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>    
-        <title>Cadastro de Li</title>
+        <title>Categorias</title>
     </head>
     <body style="background-color: gainsboro">
     <div class="page-header" align="center" >
-        <h1>Cadastro</h1>
+        <h1>Cadastro de Categoria</h1>
     </div>
     <div class="row">
         <div class="col-md-4">
--- dados.php
+++ dados.php
@@ -7,11 +7,11 @@
     <head>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>    
-        <title>Linguagens</title>
+        <title>Categorias</title>
     </head>
     <body style="background-color: #e9ecef">
         <div class="page-header" align="center" >
-            <h1>Lista de Linguagens</h1>
+            <h1>Lista de Categorias</h1>
         </div>
         <div align="center" >
             <a href="dashboard.php" class="btn btn-secondary"> Voltar </a>
@@ -78,7 +78,7 @@
 
                         $_SESSION['session'] = $_POST['editar'];
 
-                        header('Location: dashboard.php');
+                        header('Location: editar.php');
                     }  
                 ?>
                 </tbody>
